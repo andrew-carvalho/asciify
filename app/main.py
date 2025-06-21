d1dfd793c2f02781638042acd099185287a1b65d
Andrew Ignácio <andrewignacio@outlook.com.br>
2025-06-21 18:06:15 -0300

    refactor: extract argument parsing to another function

--- app/main.py
+++ app/main.py
@@ -4,20 +4,9 @@ from constants import MAX_WIDTH, MAX_HEIGHT
 from convert import get_brightness_value_from_pixel, get_char_from_brightness_value, transform_image_to_ascii_2d_array
 
 def main():
-    parser = ArgumentParser(description="A simple program that convert image to ASCII")
-
-    parser.add_argument("filename", help="The image path to process")
-    parser.add_argument("-wt", "--width", help="The width of the ASCII art output", default=MAX_WIDTH)
-    parser.add_argument("-ht", "--height", help="The height of the ASCII art output", default=MAX_HEIGHT)
-
-    args = parser.parse_args()
+    args = define_arguments()
 
     filename = args.filename
-
-    if filename is None:
-        print("Please inform the image path.")
-        return
-    
     image = None
     try:
         image = Image.open(filename)
@@ -28,6 +17,15 @@ def main():
     image = image.resize((int(args.width), int(args.height)))
     image_array = transform_image_to_ascii_2d_array(image)
     print_ascii(image, image_array)
+
+def define_arguments():
+    parser = ArgumentParser(description="A simple program that convert image to ASCII")
+
+    parser.add_argument("filename", help="The image path to process")
+    parser.add_argument("-wt", "--width", help="The width of the ASCII art output", default=MAX_WIDTH)
+    parser.add_argument("-ht", "--height", help="The height of the ASCII art output", default=MAX_HEIGHT)
+
+    return parser.parse_args()
 
 def print_ascii(image, image_array):
     for y in range(0, image.height):
