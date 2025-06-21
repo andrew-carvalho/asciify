81a1d37f8a34c09fda633a125bd31a8639556aea
Andrew Ignácio <andrewignacio@outlook.com.br>
2025-06-21 15:45:27 -0300

    feat: add image loading

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
+import PIL.Image
+
 def main():
-    pass
+    try:
+        image = PIL.Image.open("example.jpg")
+        print("Successfully loaded image!")
+        print("Image size: " + str(image.width) + " x " + str(image.height))
+    except OSError:
+        print("Error while loading image!")
 
 if __name__ == "__main__":
     main()
