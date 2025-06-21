8c853e17c4198e4cf1a2d3ec3232e1f9a3c71f03
Andrew Ignácio <andrewignacio@outlook.com.br>
2025-06-21 18:08:18 -0300

    style: format code with Black formatter

--- app/constants.py
+++ app/constants.py
@@ -1,4 +1,4 @@
-CHAR_MAP = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+CHAR_MAP = '`^",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
 RGB_RANGE = 255
 MAX_WIDTH = 640
 MAX_HEIGHT = 480
--- app/convert.py
+++ app/convert.py
@@ -1,5 +1,6 @@
 from constants import CHAR_MAP, RGB_RANGE, CHAR_SCALE
 
+
 def transform_image_to_ascii_2d_array(image):
     image_data = image.getdata()
 
@@ -8,15 +9,19 @@ def transform_image_to_ascii_2d_array(image):
     for y in range(0, image.height):
         image_array.append([])
         for x in range(0, image.width):
-            brightness_value = get_brightness_value_from_pixel(image_data.getpixel((x,y)))
+            brightness_value = get_brightness_value_from_pixel(
+                image_data.getpixel((x, y))
+            )
             char = get_char_from_brightness_value(brightness_value)
             image_array[y].append(char * CHAR_SCALE)
 
     return image_array
 
+
 # Average method -> average the R, G and B values - (R + G + B) / 3
 def get_brightness_value_from_pixel(rgb_values):
     return sum(rgb_values) / len(rgb_values)
+
 
 def get_char_from_brightness_value(brightness_value):
     map_length = len(CHAR_MAP)
