e08e252bd75787573d27d4c1ecae4be5fbfc9901
Andrew Ignácio <andrewignacio@outlook.com.br>
2025-06-21 18:03:14 -0300

    refactor: extract convertions functions to another file

---
+++ app/convert.py
@@ -0,0 +1,25 @@
+from constants import CHAR_MAP, RGB_RANGE, CHAR_SCALE
+
+def transform_image_to_ascii_2d_array(image):
+    image_data = image.getdata()
+
+    image_array = []
+
+    for y in range(0, image.height):
+        image_array.append([])
+        for x in range(0, image.width):
+            brightness_value = get_brightness_value_from_pixel(image_data.getpixel((x,y)))
+            char = get_char_from_brightness_value(brightness_value)
+            image_array[y].append(char * CHAR_SCALE)
+
+    return image_array
+
+# Average method -> average the R, G and B values - (R + G + B) / 3
+def get_brightness_value_from_pixel(rgb_values):
+    return sum(rgb_values) / len(rgb_values)
+
+def get_char_from_brightness_value(brightness_value):
+    map_length = len(CHAR_MAP)
+    scale = RGB_RANGE / map_length
+    index = int(brightness_value / scale) % map_length
+    return CHAR_MAP[index]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from argparse import ArgumentParser
-from constants import CHAR_MAP, RGB_RANGE, MAX_WIDTH, MAX_HEIGHT, CHAR_SCALE
+from constants import MAX_WIDTH, MAX_HEIGHT
+from convert import get_brightness_value_from_pixel, get_char_from_brightness_value, transform_image_to_ascii_2d_array
 
 def main():
     parser = ArgumentParser(description="A simple program that convert image to ASCII")
@@ -27,30 +28,6 @@ def main():
     image = image.resize((int(args.width), int(args.height)))
     image_array = transform_image_to_ascii_2d_array(image)
     print_ascii(image, image_array)
-
-def transform_image_to_ascii_2d_array(image):
-    image_data = image.getdata()
-
-    image_array = []
-
-    for y in range(0, image.height):
-        image_array.append([])
-        for x in range(0, image.width):
-            brightness_value = get_brightness_value_from_pixel(image_data.getpixel((x,y)))
-            char = get_char_from_brightness_value(brightness_value)
-            image_array[y].append(char * CHAR_SCALE)
-
-    return image_array
-
-# Average method -> average the R, G and B values - (R + G + B) / 3
-def get_brightness_value_from_pixel(rgb_values):
-    return sum(rgb_values) / len(rgb_values)
-
-def get_char_from_brightness_value(brightness_value):
-    map_length = len(CHAR_MAP)
-    scale = RGB_RANGE / map_length
-    index = int(brightness_value / scale) % map_length
-    return CHAR_MAP[index]
 
 def print_ascii(image, image_array):
     for y in range(0, image.height):
